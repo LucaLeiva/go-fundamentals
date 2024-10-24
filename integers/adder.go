170c663b66813618dc23c7259ca96455099aefa7
Luca Leiva <luca_nic@hotmail.com>
2024-10-23 22:54:38 -0300

    Agrego tests con Integers

---
+++ integers/adder.go
@@ -0,0 +1,6 @@
+package integers
+
+// Suma dos integers y retorna el resultado de ellos
+func Add(x, y int) int {
+	return x + y
+}
---
+++ integers/adder_test.go
@@ -0,0 +1,22 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+	// con este comentario output se valida el resultado de la funcion example y queda en el GoDoc que genera
+}
